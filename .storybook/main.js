73f3bdd36440578c86d0487b21ab2943f8b5a80d
Karthik <kingomarigold@yahoo.com>
2024-04-26 11:42:14 +0530

    Added main and preview for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,20 @@
-const path = require('path');
-
-// Export a function. Accept the base config as the only param.
-module.exports = {
-  stories: ["../src/**/*.story.jsx"],
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../src'),
-    });
-
-    // Return the altered config
-    return config;
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
   },
-};
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
