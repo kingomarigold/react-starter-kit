864bde1bc8f0c559adfb7a2c78c5ecc06b859dd1
Karthik <kingomarigold@yahoo.com>
2024-04-26 13:03:35 +0530

    Used redux toolkit for redux integration

---
+++ src/reducers/UserSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  loggedIn: false,
+  name: '',
+  token: '',
+  roles: ''
+}
+
+export const UserSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      console.log('In reducer, Login - ', action)
+      state.loggedIn = true
+      state.name = action.payload.name
+      state.token = action.payload.token
+      state.roles = action.payload.roles
+    },
+    logout: (state) => {
+      console.log('In reducer, Logout')
+      state.loggedIn = false
+      state.name = ''
+      state.token = ''
+      state.roles = ''
+    },
+    refreshToken: (state, action) => {
+      state.token = action.payload.token
+    }
+  }
+})
+
+export const { login, logout, refreshToken } = UserSlice.actions
+
+export const userName = (state) => state.user.name
+export const token = (state) => state.user.token
+export const roles = (state) => state.user.roles
+export const loggedIn = (state) => state.user.loggedIn
+export default UserSlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,23 @@
 import createSagaMiddleware from 'redux-saga'
-import { createStore, applyMiddleware } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import reducers from '../reducers'
+import userReducer from '../reducers/UserSlice'
 import rootSagas from '../sagas'
 
 const sagaMiddleware = createSagaMiddleware()
-let middleware = applyMiddleware(sagaMiddleware)
-
-// adding redux devtools for developmennt mode
-if (__DEVELOPMENT__) {
-  middleware = composeWithDevTools(middleware)
-}
+let middleware = [sagaMiddleware]
 
 // eslint-disable-next-line no-underscore-dangle
 const initialState = !__SERVER__ ? window.__PRELOADED__STATE__ : {}
 
-const store = createStore(reducers, initialState, middleware)
+const store = configureStore({
+  reducer: {
+    user: userReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(middleware),
+  composeWithDevTools: __DEVELOPMENT__?true:false,
+})
 
 if (__SERVER__) {
   store.runSaga = sagaMiddleware.run
